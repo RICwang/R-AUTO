9de180afd5e06e0fd489ea6f20a16047da920234
Ricky Wang <ricwang1987@qq.com>
2024-11-06 16:09:59 +0800

    初始化，已适配v2ex和微软

---
+++ lib/tslib.py
@@ -0,0 +1,26 @@
+# 推送类
+# 作者：Ricky
+# 日期：2024-11-22
+
+from urllib.parse import quote
+import requests
+
+class Ts:
+    def __init__(self, ts_types):
+        self.ts_types = ts_types
+        
+        pass
+
+    def send(self, content):
+        self.ts_types.forEach(lambda ts_type: self.ts_type(content))
+
+
+    # 推送通知
+    # https://api.day.app/[token]/这里改成你自己的推送内容
+    def bark_ts(self, content):
+        # 1.推送内容，并urlencode编码
+        content = quote(content)
+        # 2.推送url,content参数需要urlencode编码
+        url = 'https://api.day.app/[token]/' + content
+        # 3.发送请求
+        response = requests.get(url=url)
---
+++ main.py
@@ -0,0 +1,208 @@
+# 导入
+import multiprocessing
+import subprocess
+import os
+import json
+import time
+import logging
+import logging.handlers
+
+from func.common import *
+from lib.AutoV2ex import AutoV2ex
+from lib.AutoMicrosoft import AutoMicrosoft
+
+# 项目根目录
+rootPath = os.path.dirname(__file__)
+
+# 配置文件
+configFile = os.path.join(rootPath, 'volume', 'config', 'config.json')
+# 读取json文件
+config = json.load(open(configFile, 'r', encoding='utf-8'))
+configDebug = config["debug"]
+
+# 创建目录
+logFolder = os.path.join(rootPath, 'volume', 'logs')
+if not os.path.exists(logFolder):
+    os.makedirs(logFolder)
+# 日志格式
+logFormat = '%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(message)s'
+maxBytes=20*1024*1024
+backupCount=5
+
+# 创建主进程的日志对象
+logFile = os.path.join(logFolder, f"logger-main.log")
+loggerMain = logging.getLogger()
+logLevel = logging.DEBUG if configDebug else logging.INFO
+loggerMain.setLevel(logLevel)
+formatter = logging.Formatter(logFormat)
+fileHandler = logging.handlers.RotatingFileHandler(logFile, maxBytes=maxBytes, backupCount=backupCount, encoding='utf-8')
+fileHandler.setFormatter(formatter)
+loggerMain.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+loggerMain.addHandler(consoleHandler)
+
+dataFolder = os.path.join(rootPath, 'volume', 'data')
+if not os.path.exists(dataFolder):
+    os.makedirs(dataFolder)
+dataLogFile = os.path.join(dataFolder, 'log.json')
+
+# 初始化
+def initialize():
+    loggerMain.info(f'正在初始化数据...')
+    # 初始化data操作日志文件
+    dataLog = json.load(open(dataLogFile, 'r', encoding='utf-8'))
+    if "task" not in dataLog:
+        dataLog["task"] = dict()
+    for task in config["taskList"]:
+        k = f'{task["name"]}=={task["username"]}'
+        if k not in dataLog["task"]:
+            dataLog["task"][k] = {
+                "status": 0,
+                "port": 0,
+                "handlingDate": "",
+                "finishDates": []
+            }
+    json.dump(dataLog, open(dataLogFile, 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+    loggerMain.info(f'初始化数据完成')
+
+# 提供web服务
+def start_flask_app():
+    loggerMain.info('正在启动web服务...')
+    appPath = os.path.join(rootPath, 'app.py')
+    with open(os.devnull, 'w') as devnull:
+        subprocess.Popen(['python', appPath], stdout=devnull, stderr=devnull)
+
+# 消费者，执行任务
+def task_worker(taskname, username, password, port):
+    loggerMain.info(f'【{taskname}】任务开始')
+    if username is None or password is None:
+        loggerMain.info(f'用户名和密码没配置')
+        return
+    if port is None:
+        loggerMain.info(f'端口没配置')
+        return
+    clz = None
+    if taskname == "v2ex":
+        clz = AutoV2ex(username=username, password=password, port=port, logger=loggerMain)
+    elif taskname == "Microsoft":
+        clz = AutoMicrosoft(username=username, password=password, port=port, logger=loggerMain)
+
+    if clz is None:
+        return
+    
+    while True:
+        try:
+            clz.main()
+            if clz.res:
+                loggerMain.info(f'【{taskname}】任务已完成')
+                
+                updateDictNew = {
+                    'status': 0,
+                    'port': 0,
+                    "handlingDate": "",
+                    "finishDate": time.strftime("%Y%m%d", time.localtime())
+                }
+                update_data_log(taskname=taskname, username=username, updateDict=updateDictNew)
+                break
+        except Exception as e:
+            # 将e所有内容都输出到日志
+            loggerMain.error(e, exc_info=True)
+
+
+# 主函数
+def main():
+
+    loggerMain.info('开始任务')
+
+    # 创建一个进程字典，port为key，进程为value
+    processes = {}
+
+    # 无限循环，每20秒执行一次
+    while True:
+        todayStr = time.strftime("%Y%m%d", time.localtime())
+        timeStr = time.strftime("%H:%M:%S", time.localtime())
+
+        # 判断是否在执行时间内
+        if timeStr <= config["taskTime"][0] or timeStr >= config["taskTime"][1]:
+            time.sleep(20)
+            continue
+
+        # 遍历检查端口是否被占用
+        used_ports, unused_ports = check_port(config["ports"])
+        loggerMain.debug(f'已使用端口：{used_ports}，未使用端口：{unused_ports}')
+
+        # 读取json文件
+        dataLog = json.load(open(dataLogFile, 'r', encoding='utf-8'))
+        for k, task in dataLog["task"].items():
+            kArray = k.split('==')
+            taskname = kArray[0]
+            username = kArray[1]
+            finishDates = task["finishDates"] 
+            # 执行完成的任务不再执行
+            if todayStr in finishDates:
+                continue
+            
+            # 任务正在执行中
+            if task["status"] == 1:
+                if task["port"] in unused_ports:
+                    loggerMain.info(f'【{taskname}】端口【{task["port"]}】被释放，重启服务')
+                    updateDictNew = {
+                        "status" : 0,
+                        "port" : 0,
+                        "handlingDate" : ""
+                    }
+                    update_data_log(taskname=taskname, username=username, updateDict=updateDictNew)
+                    continue
+
+                handlingDate = task["handlingDate"]
+                if todayStr != handlingDate:
+                    # 今天之前的任务还在执行中，杀掉任务重启
+                    if task["port"] in processes:
+                        processes[task["port"]].terminate()
+                        processes.pop(task["port"])
+
+                    updateDictNew = {
+                        "status" : 0,
+                        "port" : 0,
+                        "handlingDate" : ""
+                    }
+                    update_data_log(taskname=taskname, username=username, updateDict=updateDictNew)
+                    loggerMain.info(f'重置【{taskname}】进程')
+                    continue
+
+            # 任务未执行
+            if task["status"] == 0:
+                loggerMain.info(f'【{taskname}】开始任务')
+                # 更新config.json文件，将status置为1
+                updateDictNew = {
+                    "status": 1,
+                    "handlingDate" : todayStr
+                }
+                # 从unused_ports取出一个端口，并从unused_ports中删除
+                port = unused_ports.pop(0)
+                updateDictNew["port"] = port
+                
+                update_data_log(taskname=taskname, username=username, updateDict=updateDictNew)
+                configTask = get_config_task(taskname=taskname, username=username)
+
+                # 创建进程
+                p = multiprocessing.Process(target=task_worker, args=(taskname, username, configTask["password"], port))
+                # 启动进程
+                p.start()
+                # 将进程加入到进程列表中
+                processes[port] = p
+            
+        time.sleep(20)
+
+
+if __name__ == '__main__':
+    loggerMain.info(f'欢迎使用【{config["projectName"]}】项目，当前版本：【{config["version"]}】')
+    initialize()
+
+    # 启动Flask应用
+    flaskProcess = multiprocessing.Process(target=start_flask_app)
+    flaskProcess.start()
+    # pass
+    main()
